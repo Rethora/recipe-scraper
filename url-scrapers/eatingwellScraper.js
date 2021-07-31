3d12ff779cba4dfbd6a2cc77d106ba0696a08704
Dane Roelofs <dane.l.roelofs@gmail.com>
2021-07-31 12:49:38 -0700

    Fixed Eatingwell Scraper

--- url-scrapers/eatingwellScraper.js
+++ url-scrapers/eatingwellScraper.js
@@ -5,8 +5,9 @@ module.exports = html => {
     const urls = [];
     $('a.card__titleLink').each((i, el) => urls.push(el.attribs.href));
 
-    if (urls.length === 0)
-        $('div.paragraph > p > a').each((idx, el) => urls.push(el.attribs.href));
+    $('div.paragraph > p > a').each((idx, el) => urls.push(el.attribs.href));
+
+    $('div.glide-slide-cta > a').each((i, el) => urls.push($(el).attr('href')));
 
     return [...new Set(urls)];
 };
